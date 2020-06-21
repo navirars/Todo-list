a832fda4a21190ebddddc5f63b2be6f374d61a69
sultan254 <abubakarali3w@gmail.com>
2020-06-21 23:03:20 +0300

    speed optimazation

--- app.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(e){
-    $(".txt").on("keyup",function(e){
-        if (e.keyCode == 13 && $(".txt").val()!= "")
-        {
-            var task = $("<div class= "task"></div>").text(".txt").val());
-            $("Not").append(task);
-        }
-    });
-});
